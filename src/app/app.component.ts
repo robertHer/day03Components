b9a308b565408e31b8906aeae223f1b72c00b1f4
unknown <totolero769@gmail.com>
2018-06-06 11:19:30 -0700

    added random button that calls each component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-}
+
+  randomComponent:boolean = false;
+index:number;
+
+  selectComponent(){
+    this.randomComponent = true;
+    // const index = Math.floor(Math.random()*3);
+    this.index = Math.floor(Math.random()*3);
+  //   if(!index){
+  //     console.log('warning')
+  //   }
+  //   else if (index == 1){
+
+  //   }
+  //   else if(index == 2){}
+  // }
+    }}
--- src/app/warning-message/warning-message.component.ts
+++ src/app/warning-message/warning-message.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'warning-message',
-  templateUrl: './warning-message.component.html',
+  templateUrl:'./warning-message.component.html',
   styleUrls: ['./warning-message.component.css']
 })
 export class WarningMessageComponent implements OnInit {
